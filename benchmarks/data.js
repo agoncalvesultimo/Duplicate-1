e108dec071bf53bcfa72a28934c9f3b168d0d1d4
github-action-benchmark <github@users.noreply.github.com>
2025-09-14 18:45:38 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 277e603bec5a6a96cd5fd522edb22cb28fafc79b

---
+++ benchmarks/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1757875538133,
+  "repoUrl": "https://github.com/agoncalvesultimo/Duplicate-1",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "alvaro.goncalves@ultimo.com",
+            "name": "agoncalvesultimo",
+            "username": "agoncalvesultimo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "277e603bec5a6a96cd5fd522edb22cb28fafc79b",
+          "message": "Add files via upload",
+          "timestamp": "2025-09-14T20:16:27+02:00",
+          "tree_id": "90219594d943c30a104570e6e8c04b747b010685",
+          "url": "https://github.com/agoncalvesultimo/Duplicate-1/commit/277e603bec5a6a96cd5fd522edb22cb28fafc79b"
+        },
+        "date": 1757875537676,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0192,
+            "range": "0.2467",
+            "unit": "ms",
+            "extra": "52126 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1597,
+            "range": "0.4721000000000002",
+            "unit": "ms",
+            "extra": "316 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0046,
+            "range": "0.247",
+            "unit": "ms",
+            "extra": "215398 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0655,
+            "range": "0.27799999999999997",
+            "unit": "ms",
+            "extra": "15263 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
